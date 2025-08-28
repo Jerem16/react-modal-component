c307a2262f4682d4e8077e7ba819b78914a98c59
Jerem16 <jeremy.lemaignent@gmail.com>
2025-08-28 19:02:49 +0200

    Start

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,17 @@
-import resolve from "@rollup/plugin-node-resolve";
-import babel from "@rollup/plugin-babel";
-import postcss from "rollup-plugin-postcss";
-import terser from "@rollup/plugin-terser";
-import dts from "rollup-plugin-dts";
+// rollup.config.js (version CommonJS)
+const resolve = require("@rollup/plugin-node-resolve");
+const babel = require("@rollup/plugin-babel");
+const postcss = require("rollup-plugin-postcss");
+const { terser } = require("@rollup/plugin-terser");
+const dts = require("rollup-plugin-dts").default;
 
-export default [
+module.exports = [
     {
         input: "src/index.js",
         output: [
-            {
-                file: "dist/index.js",
-                format: "cjs",
-            },
-            {
-                file: "dist/index.esm.js",
-                format: "esm",
-            },
-            {
-                file: "dist/index.min.js",
-                format: "cjs",
-            },
+            { file: "dist/index.js", format: "cjs" },
+            { file: "dist/index.esm.js", format: "esm" },
+            { file: "dist/index.min.js", format: "cjs" },
         ],
         external: ["react", "react-dom"],
         plugins: [
